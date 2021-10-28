a52bebcc9a1016052dbc6751955225759b63e011
Seeminglone <yurakravtsov833@gmail.com>
2021-10-29 01:37:39 +0200

    SampleCode

---
+++ script.js
@@ -0,0 +1,83 @@
+// Декларация переменных
+// var first = 'rr32';
+// let second = '';
+// const third = 'fewfewf';
+
+
+// Мутация объекта который задекларировали при помощи ключевого слова const 
+// const objectYurs = {
+//     age: 22
+// }
+
+// console.log(objectYurs);
+// objectYurs.name = 'Sanya';
+// console.log(objectYurs);
+
+// error
+// objectYurs = {
+//     age: 33
+// }
+
+// its ok
+// objectYurs.age = 33;
+// console.log(objectYurs);
+
+
+
+// Типы данных
+// let string = 'This is our string';
+// let number = 33;
+// let bigInteger = 572347658934625934875893465897234654389;
+// let boolean = false, secondBoolean = true;
+// let ourNull = null;
+// let ourUndefined = undefined;
+// let object = {};
+// // symbol
+// // typeof - проверяет какой тип данных
+// console.log(typeof 'this is string');
+
+
+
+
+// Преобразование типов
+// console.log(2 + '13'); // '213'
+// console.log('6' / '2'); // 3
+// console.log(true + false); // 1
+// console.log(44 + undefined); // NaN
+// // falsy values in js: undefined, null, NaN, 0, "" = false
+
+
+
+//
+// let firstNumber = +prompt('Enter your first number:');
+// let secondNumber = +prompt('Enter your second number:');
+
+// document.write('Your answer: ' + (firstNumber + secondNumber));
+
+
+
+
+
+
+
+
+
+// let mainContainer = '4to ', footerContainer = 'tam?';
+// console.log(mainContainer + footerContainer);
+
+// let a = 2;
+// let b = 3;
+// const c = 5;
+// const d = 1;
+
+// console.log((a + b) * c / d);
+
+// a = 5;
+// b = 2;
+
+// document.write('Your answer: ' + ((a + b) * c / d));
+
+
+// let container = '6' / '3';
+// console.log(typeof container);
+
